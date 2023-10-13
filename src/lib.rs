0c44dce0c1259170e862859d8b42f86014886563
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-10-13 15:58:24 +0200

    Working(?) unlock

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,15 @@ use std::str::FromStr;
 use anyhow::Result;
 use log::{debug, error, info};
 use pamsm::{Pam, PamError, PamFlags, PamLibExt, PamServiceModule};
+use serde::Deserialize;
 
 struct PamLoadKeys;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct BitwardenStatusOutput {
+    status: String
+}
 
 impl PamServiceModule for PamLoadKeys {
     fn open_session(pam: Pam, _flags: PamFlags, args: Vec<String>) -> PamError {
@@ -19,11 +26,40 @@ impl PamServiceModule for PamLoadKeys {
 
         match pam.get_user(None) {
             Ok(Some(user)) => {
-                let output = run_bw(user, "status").unwrap();
+                let output = run_bw(user, vec!["status"], None).unwrap();
 
                 debug!("bw status: {}", str::from_utf8(output.stdout.as_slice()).unwrap());
 
-                info!("Hello, {:?}!", user);
+                let status: BitwardenStatusOutput = serde_json::from_slice(output.stdout.as_slice()).unwrap();
+
+                if status.status == "locked" {
+                    debug!("Attempting unlock ...");
+
+                    if let Ok(password) = pam.get_authtok(None) {
+                        match run_bw(user, vec!["unlock", "--raw"], password) {
+                            Ok(session_output) => {
+                                debug!("Successfully unlocked vault");
+                                match str::from_utf8(session_output.stdout.as_slice()) {
+                                    Ok(session_id) => {
+                                        debug!("Setting BW_SESSION={}", session_id);
+                                        if let Err(_) = pam.putenv(format!("BW_SESSION={}", session_id).as_str()) {
+                                            error!("Failed to set BW_SESSION environment variable");
+                                            return PamError::SYSTEM_ERR;
+                                        }
+                                    }
+                                    Err(e) => {
+                                        error!("Failed to parse session id: {:?}", e);
+                                        return PamError::AUTH_ERR;
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                error!("Failed to unlock vault: {:?}", e);
+                                return PamError::AUTH_ERR;
+                            }
+                        }
+                    }
+                }
             }
             Ok(None) => {
                 error!("ERROR: get_user returned None, should not happen!");
@@ -35,7 +71,6 @@ impl PamServiceModule for PamLoadKeys {
             }
         }
 
-        let _password = pam.get_authtok(None).unwrap();
         PamError::SUCCESS
     }
 
@@ -47,14 +82,22 @@ impl PamServiceModule for PamLoadKeys {
     }
 }
 
-fn run_bw(user: &CStr, cmd: &str) -> Result<Output> {
+fn run_bw(user: &CStr, cmd: Vec<&str>, password: Option<&CStr>) -> Result<Output> {
+    let password_args = match password {
+        Some(_) => vec!["--passwordenv", "BW_PASSWORD"],
+        None => vec![],
+    };
+    let pw = password.map(|p| p.to_str().unwrap()).unwrap_or("");
     let child = Command::new("/usr/bin/sudo")
+        .env("BW_PASSWORD", pw)
+        .arg("--preserve-env=BW_PASSWORD")
         .arg("--non-interactive")
         .arg("--set-home")
         .arg("--user")
         .arg(user.to_str().unwrap())
         .arg("/snap/bin/bw")
-        .arg(cmd)
+        .args(cmd)
+        .args(password_args)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
@@ -62,6 +105,13 @@ fn run_bw(user: &CStr, cmd: &str) -> Result<Output> {
     let output = child
         .wait_with_output()
         .map_err(|e| anyhow::anyhow!("failed to wait on child: {}", e))?;
+    if !output.status.success() {
+        Err(anyhow::anyhow!(
+            "command exited with status: {}",
+            output.status
+        ))?
+    }
+
     Ok(output)
 }
 
@@ -79,7 +129,7 @@ fn init_logging(args: &Vec<String>) {
             writeln!(buf, "pam_bitwarden: {}: {}: {}", ts, record.level(), record.args())
         })
         .init();
-    info!("Logging initialized");
+    debug!("Logging initialized");
 }
 
 pam_module!(PamLoadKeys);
